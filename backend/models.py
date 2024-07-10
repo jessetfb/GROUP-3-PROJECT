5fb99398eb9e5a35f6b6d7b3ab0fe87a932d2605
Matilda <ogarimatilda@gmail.com>
2024-07-10 22:08:35 +0300

    models

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,8 @@
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, DateTime,func
 from sqlalchemy.ext.associationproxy import association_proxy
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+
 
 metadata = MetaData(
     naming_convention={
@@ -12,7 +13,7 @@ metadata = MetaData(
 db = SQLAlchemy(metadata=metadata)
 
 #model
-class Car(db.Model, SerializerMixin):
+class Car(db.Model):
     __tablename__ = 'cars'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -23,16 +24,18 @@ class Car(db.Model, SerializerMixin):
     fuel_type = db.Column(db.String)
     horse_power = db.Column(db.Integer)
     transmission = db.Column(db.String)
+    cost = db.Column(db.Integer, nullable=False)
 
     bookings = db.relationship('Booking', back_populates ='car')
 
-class Booking(db.Model, SerializerMixin):
+class Booking(db.Model):
     __tablename__ = 'booking'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     car_id = db.Column(db.Integer, db.ForeignKey('cars.id'))
-    start_date = db.Column(db.Date)
-    end_date = db.Column(db.Date)
+    cost = db.Column(db.Integer, nullable=False)
+    start_date = db.Column (DateTime, nullable=False)
+    end_date = db.Column (DateTime, nullable=False)
 
     user = db.relationship('User', back_populates ='bookings')
     car = db.relationship('Car', back_populates = 'bookings')
@@ -46,3 +49,14 @@ class User(db.Model, SerializerMixin):
     password = db.Column(db.String)
 
     serialize_rules = ('-password',)
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    car_id = db.Column(db.Integer, db.ForeignKey('cars.id'), nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+    rating = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.Text)
+    created_at = db.Column(DateTime, default=func.now())
+    
+    car = db.relationship('Car', back_populates = 'reviews')
+    user = db.relationship('User', back_populates = 'reviews')
