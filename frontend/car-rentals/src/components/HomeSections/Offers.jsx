59c6676e9a5705a9e1a34f7f823dcb00c0f8bcff
Aloice Ogana <aloiceadriot@gmail.com>
2024-07-11 14:33:40 +0300

    banner-jsx-offer.add

--- frontend/car-rentals/src/components/HomeSections/Offers.jsx
+++ frontend/car-rentals/src/components/HomeSections/Offers.jsx
@@ -1,10 +1,10 @@
-import Container from '../Container';
+import Container from "../Container";
 
 const Offers = () => {
   return (
     <section className="px-4">
       <Container className="m-auto flex max-w-[1440px] flex-wrap justify-between gap-4">
-        {' '}
+        {" "}
         <p className="rounded-lg  p-2 text-lg font-bold text-main-text dark:text-white">
           Competitive Rates
         </p>
@@ -21,5 +21,5 @@ const Offers = () => {
     </section>
   );
 };
-
+//
 export default Offers;
