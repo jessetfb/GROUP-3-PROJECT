7323b75c93e1090f9a2c5e909685d05b2c515a39
Aloice Ogana <aloiceadriot@gmail.com>
2024-07-19 02:23:28 +0300

    banner-jsx-home.+texts

--- frontend/car-rentals/src/App.jsx
+++ frontend/car-rentals/src/App.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter } from 'react-router-dom';
-import { Suspense, lazy } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 
-import Layout from './components/Layout';
-import Spinner from './components/Spinner';
+import Layout from "./components/Layout";
+import Spinner from "./components/Spinner";
 
-const HomePage = lazy(() => import('./pages/HomePage'));
-const CatalogPage = lazy(() => import('./pages/CatalogPage'));
-const FavoritesPage = lazy(() => import('./pages/FavoritesPage'));
+const HomePage = lazy(() => import("./pages/HomePage"));
+const CatalogPage = lazy(() => import("./pages/CatalogPage"));
+const FavoritesPage = lazy(() => import("./pages/FavoritesPage"));
 
 const App = () => {
   return (
