7905bd87ee83f090e6aae7bf2f1f644db459a197
jessetfb <langatjesse@gmail.com>
2024-07-26 03:37:22 +0300

    changes

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
